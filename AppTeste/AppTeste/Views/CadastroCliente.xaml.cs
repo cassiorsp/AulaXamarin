d5a9a1c91e45437db09ba57c0f026603b69c8e3d
CASSIO RODRIGO SOARES DO PRADO <cassio.rsprado@senacsp.edu.br>
2022-09-21 22:04:59 -0300

    atualização

--- AppTeste/AppTeste/Views/CadastroCliente.xaml.cs
+++ AppTeste/AppTeste/Views/CadastroCliente.xaml.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
+using AppTeste.Data;
+using AppTeste.Models;
 namespace AppTeste.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CadastroCliente : ContentPage
     {
+        DataContext objContext;
         public CadastroCliente()
         {
             InitializeComponent();
+            objContext = new DataContext();
+            BindingContext = new Cliente();
+        }
+
+        private async void SalvarCliente(object sender, EventArgs e)
+        {
+            var cliente = (Cliente)BindingContext;
+
+            await objContext.SalvarCliente(cliente);
         }
     }
 }
