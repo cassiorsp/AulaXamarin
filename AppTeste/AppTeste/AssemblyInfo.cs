8665ee1acd8cb4bc287332d88688fe96b3c7776b
CASSIO RODRIGO SOARES DO PRADO <cassio.rsprado@senacsp.edu.br>
2022-09-27 19:26:57 -0300

    atualização

--- AppTeste/AppTeste/AssemblyInfo.cs
+++ AppTeste/AppTeste/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("angelina.TTF", Alias = "angelina")]
+[assembly: ExportFont("LOKICOLA.TTF", Alias = "lokicola")]
+[assembly: ExportFont("waltographUI.ttf", Alias = "waltographUI ")]
